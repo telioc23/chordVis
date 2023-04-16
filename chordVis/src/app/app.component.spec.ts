d9b168038bfe62640ee590c3157f4a1a9b8f864e
telioc <rschuller23@gmail.com>
2023-04-16 14:08:53 +0100

    adding tests

--- chordVis/src/app/app.component.spec.ts
+++ chordVis/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Chord } from '@tonaljs/tonal';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -32,4 +33,11 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('chordVis app is running!');
   });
+
+  it("test1", ()=> {
+    const a = Chord.detect(['D', 'F#', 'A', 'C']); // => ["D7"]
+    const b = Chord.detect(['F#', 'A', 'C', 'D']); // => ["D7/F#"]
+    const c = Chord.detect(['A', 'C', 'D', 'F#']); // => ["D7/A"]
+    const d = Chord.detect(['E', 'G#', 'B', 'C#']); // => ["E6", "C#m7/E"]
+  }) 
 });
