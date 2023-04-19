a02966db1dadc4bec7fbc51f6af0870746754eb5
telioc <rschuller23@gmail.com>
2023-04-19 16:35:06 +0100

    kinda working

--- chordVis/src/app/spiral-svg/spiral-svg.component.ts
+++ chordVis/src/app/spiral-svg/spiral-svg.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-spiral-svg',
   templateUrl: './spiral-svg.component.html',
   styleUrls: ['./spiral-svg.component.scss'],
 })
-export class SpiralSvgComponent implements OnInit {
-  svg = document.getElementsByTagName('svg')[0];
+export class SpiralSvgComponent implements AfterViewInit {
+  @ViewChild('svg') svg!: ElementRef<SVGElement>;
 
   createOn(root: SVGElement, name: string, attrs: any) {
     var el = document.createElementNS('http://www.w3.org/2000/svg', name);
@@ -17,25 +17,25 @@ export class SpiralSvgComponent implements OnInit {
   }
 
   //https://codepen.io/cyanos/pen/XbXxOQ
-  ngOnInit(): void {
-    this.createOn(this.svg, 'path', {
+  ngAfterViewInit(): void {
+    this.createOn(this.svg.nativeElement, 'path', {
       fill: 'none',
       class: 'face',
       transform: 'translate(-396,-230)',
       d: 'M487.41,282.411c-15.07,36.137-50.735,61.537-92.333,61.537 \
             c-41.421,0-76.961-25.185-92.142-61.076',
     });
-    this.createOn(this.svg, 'circle', {
+    this.createOn(this.svg.nativeElement, 'circle', {
       cx: -60,
       cy: -50,
       r: 10,
       fill: 'yellow',
     });
-    this.createOn(this.svg, 'circle', {
+    this.createOn(this.svg.nativeElement, 'circle', {
       cx: 60,
       cy: -50,
       r: 10,
-      fill: '#yellow',
+      fill: 'yellow',
     });
   }
 }
